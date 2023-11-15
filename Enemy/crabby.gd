e99d9f0b3ff68688a147fc5a2ad11f74a2b3a122
Hakis1 <t1hajo02@students.oamk.fi>
2023-11-15 20:38:24 +0200

    28-enemy-attack: Enemy voi kuolla, hyökkäys toteutetaan pelaajan tiedostoon

--- Enemy/crabby.gd
+++ Enemy/crabby.gd
@@ -5,11 +5,15 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
 var chase = false
 
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
 func _physics_process(delta):
 	#Gravity for the crabby
 	velocity.y += gravity * delta
 	if chase == true:
-		get_node("AnimatedSprite2D").play("Jump")
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Jump")
 		player = get_node("../../Player/Player")
 		var direction = (player.position - self.position).normalized()
 		if direction.x > 0:
@@ -18,7 +22,8 @@ func _physics_process(delta):
 			get_node("AnimatedSprite2D").flip_h = false
 		velocity.x = direction.x * SPEED
 	else:
-		get_node("AnimatedSprite2D").play("Idle")
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")
 		velocity.x = 0
 	move_and_slide()
 
@@ -31,3 +36,11 @@ func _on_player_detection_body_entered(body):
 func _on_player_detection_body_exited(body):
 	if body.name == "Player":
 		chase = false
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		chase = false
+		get_node("AnimatedSprite2D").play("Death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free()
