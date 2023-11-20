064a217cd5d5b6636bdb1053ff7fa8a0743f424a
LeeviJarvinen <t0jale00@students.oamk.fi>
2023-11-20 16:37:55 +0200

    parannettu arkku avain logiikkaa

--- Collectibles/chest.gd
+++ Collectibles/chest.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var anim = get_node("AnimationPlayer")
+var opened = false
 
 func _on_body_entered(body):
-	if body.name == "Player" and Game.playerKeys > 0:
+	if body.name == "Player" and Game.playerKeys > 0 and opened == false:
+		opened = true
 		Game.playerKeys -= 1
 		print("Keys: ", Game.playerKeys)
 		anim.play("Unlocked")
