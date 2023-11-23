d47a4dea59c1f3a1333a7986d4d5640d9b1dba8d
LeeviJarvinen <t0jale00@students.oamk.fi>
2023-11-23 15:26:48 +0200

    Level valikko tehty vielä vaati säätöä

---
+++ Menu/Level_selection/level_icon.gd
@@ -0,0 +1,20 @@
+#@tool
+extends Control
+class_name LevelIcon
+
+@export var level_name: = "1"
+@export_file("*.tscn") var next_scene_path: String
+@export var next_level_up: LevelIcon
+@export var next_level_down: LevelIcon
+@export var next_level_left: LevelIcon
+@export var next_level_right: LevelIcon
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = "LEVEL " + str(level_name)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Engine.is_editor_hint():
+		$Label.text = "LEVEL " + str(level_name)
