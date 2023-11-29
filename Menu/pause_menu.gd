2bcd1262076996826242c179de7913842a12d7b8
VeikkaK <kursulaveikka@gmail.com>
2023-11-29 19:35:40 +0200

    pause-menu toimivaksi

--- Menu/pause_menu.gd
+++ Menu/pause_menu.gd
@@ -1,26 +1,30 @@
 extends CanvasLayer
 
-func _ready():
-	self.visible = false
-	set_process_input(true)
+#var _is_paused:bool = false:
+#	set = set_paused
 
-func _input(event):
+var _is_paused:bool = false:
+	set(value):
+		_is_paused = value
+		get_tree().paused = _is_paused
+		visible = _is_paused
+
+
+func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		check_pause()
+		_is_paused = !_is_paused
 
-func check_pause():
-	var scene_tree = get_tree()
 
-	if scene_tree.paused:
-		scene_tree.set_pause(false)
-		self.visible = false
-	else:
-		scene_tree.set_pause(true)
-		self.visible = true
+func set_paused(value:bool) -> void:
+	_is_paused = value
+	get_tree().paused = _is_paused
+	visible = _is_paused
 
-func _on_resume_pressed():
-	check_pause()
+func _on_resume_pressed() -> void:
+	_is_paused = false
+	
 
-func _on_main_menu_pressed():
+func _on_main_menu_pressed() -> void:
+	_is_paused = false
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
-	check_pause()
+	
