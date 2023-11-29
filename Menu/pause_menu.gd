3602a1089676e293f9a51387ea25a02bec2b1b55
VeikkaK <kursulaveikka@gmail.com>
2023-11-29 17:07:23 +0200

    Hotfix

--- Menu/pause_menu.gd
+++ Menu/pause_menu.gd
@@ -6,11 +6,21 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
+		check_pause()
+
+func check_pause():
+	var scene_tree = get_tree()
+
+	if scene_tree.paused:
+		scene_tree.set_pause(false)
+		self.visible = false
+	else:
+		scene_tree.set_pause(true)
 		self.visible = true
 
 func _on_resume_pressed():
-	self.visible = false
+	check_pause()
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
-	self.visible = false
+	check_pause()
