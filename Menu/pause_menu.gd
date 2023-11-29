db9aaf832384add216d9849403aad34f8f65130b
VeikkaK <kursulaveikka@gmail.com>
2023-11-29 16:20:48 +0200

    Hotfix

--- Menu/pause_menu.gd
+++ Menu/pause_menu.gd
@@ -2,17 +2,15 @@ extends CanvasLayer
 
 func _ready():
 	self.visible = false
-	#set_process_input(true)
+	set_process_input(true)
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		self.visible = true
 
-func resume():
-	self.visible = false
-
 func _on_resume_pressed():
-	resume()
+	self.visible = false
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
+	self.visible = false
