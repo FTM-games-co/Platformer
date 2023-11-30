363e8b0ea23d98843151a06dd1350c5294a8e7df
LeeviJarvinen <t0jale00@students.oamk.fi>
2023-11-30 15:55:05 +0200

    pelaajan kuolemattomuus ja piikkien damage toimii

--- Levels/Decor/spike.gd
+++ Levels/Decor/spike.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
 
-#func _on_body_entered(body):
-	#Game.playerHealth -= 1
+func _on_body_entered(body):
+	if body.name == "Player":
+		Game.player_damage()
--- Util/Global/Game.gd
+++ Util/Global/Game.gd
@@ -5,3 +5,19 @@ var playerHealth = 5
 var playerGold = 0
 var playerKeys = 0
 var progress = 1
+var immortality = false
+
+func player_damage():
+	if not immortality:
+		playerHealth -= 1
+		player_immortality()
+		
+func player_immortality():
+	immortality = true
+	print("immortality is on")
+	await get_tree().create_timer(2).timeout
+	disable_player_immortality()
+	
+func disable_player_immortality():
+	print("immortality is off")
+	immortality = false
