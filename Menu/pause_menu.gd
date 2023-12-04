c04c2bb415e284ac58374985effbd9fed8b37258
VeikkaK <kursulaveikka@gmail.com>
2023-12-04 16:50:18 +0200

    Pause-menu tähän vaiheeseen.

--- Menu/pause_menu.gd
+++ Menu/pause_menu.gd
@@ -1,19 +1,14 @@
 extends CanvasLayer
 
-#var _is_paused:bool = false:
-#	set = set_paused
-
 var _is_paused:bool = false:
 	set(value):
 		_is_paused = value
 		get_tree().paused = _is_paused
 		visible = _is_paused
 
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		_is_paused = !_is_paused
-
 
 func set_paused(value:bool) -> void:
 	_is_paused = value
@@ -22,12 +17,6 @@ func set_paused(value:bool) -> void:
 
 func _on_resume_pressed() -> void:
 	_is_paused = false
-	
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://Menu/options.tscn")
-	_is_paused = false
