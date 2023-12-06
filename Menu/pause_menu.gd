e3238febbb6bac61f1702f7cb951ee5ccc7ad8e5
VeikkaK <kursulaveikka@gmail.com>
2023-12-06 20:00:30 +0200

    Ehostusta ja parit bugit hoidettu.

--- Menu/pause_menu.gd
+++ Menu/pause_menu.gd
@@ -7,16 +7,31 @@ var _is_paused:bool = false:
 		visible = _is_paused
 
 func _unhandled_input(event: InputEvent) -> void:
+	var uicanvas = find_child("UI_canvas", true, false)
 	if event.is_action_pressed("ui_cancel"):
-		_is_paused = !_is_paused
-
-func set_paused(value:bool) -> void:
-	_is_paused = value
-	get_tree().paused = _is_paused
-	visible = _is_paused
-
+		_is_paused = true
+		uicanvas.visible = false
+		print("paused")
+		
+"""
+unc _unhandled_input(event: InputEvent) -> void:
+	var uicanvas = find_child("UI_canvas", true, false)
+	if event.is_action_pressed("ui_cancel"):
+		if _is_paused:
+			_is_paused = false
+			uicanvas.visible = true
+			print("resumed")
+		else:
+			_is_paused = true
+			uicanvas.visible = false
+			print("paused")
+"""		
+		
 func _on_resume_pressed() -> void:
+	var uicanvas = find_child("UI_canvas", true, false)
 	_is_paused = false
+	uicanvas.visible = true
+	print("resumed")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
