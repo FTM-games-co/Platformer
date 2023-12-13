d095e06b42b888b3aa67ae5269e26ed3136ce4fe
VeikkaK <kursulaveikka@gmail.com>
2023-12-13 19:52:36 +0200

    Parannuksia

--- Menu/deathscreen.gd
+++ Menu/deathscreen.gd
@@ -1,7 +1,14 @@
 extends CanvasLayer
+var deathsoundeffect = DeathSoundEffect
+var clickeffect = ClickEffect
+
+func _ready():
+	deathsoundeffect.play()
 
 func _on_retry_pressed():
 	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
+	clickeffect.play()
 	
 func _on_menu_pressed():
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
+	clickeffect.play()
