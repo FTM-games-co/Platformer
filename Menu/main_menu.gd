4f0b4a88af5435bc4cb66d456019d83d6227de70
Hakis1 <t1hajo02@students.oamk.fi>
2023-12-14 20:41:47 +0200

    58-main-menu-fixes: Main menu fixes, centering and adding styling

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -7,7 +7,7 @@ var deathmusic = DeathMusic
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	get_node("AnimatedSprite2D").play("Run")
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Menu/Level_selection/level_select.tscn")
