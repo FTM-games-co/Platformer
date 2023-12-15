004c034f06956ad57700c0df323ede7f814a2bdf
VeikkaK <kursulaveikka@gmail.com>
2023-12-15 14:55:24 +0200

    Key-effect yms

--- Collectibles/key.gd
+++ Collectibles/key.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
-var coineffect = CoinEffect
+var keyeffect = KeyEffect
 
 @onready var anim = get_node("AnimationPlayer")
 
 func _on_body_entered(body):
 	if body.name == "Player" and $CollisionShape2D.disabled == false:
+		keyeffect.play()
 		$CollisionShape2D.disabled = true
 		Game.playerKeys += 1
 		anim.play("Effect")
 		print("Keys: ", Game.playerKeys)
 		await get_node("AnimationPlayer").animation_finished
 		self.queue_free()
-		coineffect.play()
